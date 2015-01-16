5229ba5ed7790fb51268defbfabb1f15c5853a97
Jose Miguel Parrella <j@bureado.com>
2015-01-16 19:49:12 +0000

    Differentiating process time deltas from packet time deltas

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var socket = require('socket.io-client')('http://qssub.cloudapp.net:8080');
 
 var pkct = 0;
-var processStart;  // epoch in ms
-var processEnd;    // epoch in ms
+var packetStart;  // epoch in ms
+var packetEnd = 0;    // epoch in ms
 var fullData = {}; // 1-indexed
 var cnns = 0;
 var dscn = 0;
@@ -13,28 +13,35 @@ console.time("quake");
 socket.on("connect", function() {
 	++cnns;
 	console.log(process.pid + " socket connected");
+
 }), socket.on("message", function(t) {
 	var e = JSON.parse(t);
 
-	if ( !e.data ){
+	if (!e.data){
 		return;
 	}
 
 	pkct++;
 
-	if (!processStart) {
-		processStart = e.starttime;
+	if (!packetStart) {
+		packetStart = e.starttime;
 	}
 
-	processEnd = e.endtime;
-
+	if ( e.endtime > packetEnd ) {
+		packetEnd = e.endtime;
+	}
+/*
 	var msgl = t.toString().length;     // in bytes
 	var msgd = e.endtime - e.starttime; // in ms
 	var bptd = msgl / msgd;             // in bytes/ms
+
 	fullData[pkct] = { msgl: msgl, msgd: msgd, bptd: bptd };
+*/
+
 }), socket.on("disconnect", function() {
 	++dscn;
 	console.log(process.pid + " socket disconnected");
+
 }), socket.on("reconnect", function() {
 	++rcns;
 	console.log(process.pid + " socket reconnected");
@@ -47,18 +54,21 @@ process.on('SIGINT', function() {
 });
 
 function wrapup() {
-	if (!pkct || !processEnd || !processStart) {
+	if (!pkct || !packetEnd || !packetStart) {
 		return;
 	}
 
-	var rate = pkct/(processEnd-processStart)*1e5;
+	var rate = pkct/((packetEnd-packetStart)/1e5);
 
 	console.log("Packets      " + pkct);
-	console.log("Rate (pk/s)  " + rate);
-	console.log("Duration (s) " + (processEnd-processStart)/1e5);
+	console.log("Rate         " + rate);
+	console.log("Duration     " + (packetEnd-packetStart)/1e5);
 	console.log("Connects     " + cnns);
 	console.log("Reconnects   " + rcns);
 	console.log("Disconnects  " + dscn);
+
+	console.log("Start packet " + packetStart);
+	console.log("Final packet " + packetEnd);
 
 	console.timeEnd("quake");
 }
